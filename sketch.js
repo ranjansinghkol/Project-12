b1446d3376a3d346d19917bf2017fd6b8abcbe42
ranjansinghkol <38905611+ranjansinghkol@users.noreply.github.com>
2021-08-23 14:35:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var garden, rabbit;
 var gardenImg, rabbitImg;
-var leaf, leafImg, orangeLeaf;
+var leaf, orangeLeaf, redLeaf, leafImg, orangeLeafImg, redLeafImg;
 var apple, appleImg;
 
 var rand_sprite;
@@ -10,11 +10,11 @@ function preload(){
   rabbitImg = loadImage("rabbit.png");
   leafImg = loadImage("leaf.png");
   appleImg = loadImage("apple.png");
-  orangeLeaf = loadImage("orangeLeaf.png");
+  orangeLeafImg = loadImage("orangeLeaf.png");
+  redLeafImg = loadImage("redImage.png");
 }
 
 function setup(){
-  
   createCanvas(400,400);
   
   // background
@@ -36,36 +36,67 @@ function draw() {
 
   rabbit.x = mouseX;
 
-  createLeaves();
-  createApples();
-  
+  var sprites = Math.round(random(1, 5));
+  if(frameCount%80==0){
+    switch(sprites){
+      case 1:
+        createLeaves();
+        sprites = Math.round(random(1, 5));
+        break;
+      case 2:
+        createOrangeLeaves();
+        sprites = Math.round(random(1, 5));
+        break;
+      case 3:
+        createRedLeaves();
+        sprites = Math.round(random(1, 5));
+        break;
+      case 4:
+        createApples();
+        sprites = Math.round(random(1, 5));
+        break;
+      case 5:
+        createApples();
+        sprites = Math.round(random(1, 5));
+        break;
+      default:
+        break;
+    }
+  }
 
   drawSprites();
 }
 
 function createLeaves(){
-  var leafImgs = Math.round(random(1,2));
-  if(frameCount%80===0){
-    if(leafImgs==1){
-      leaf = createSprite(random(50, 350), 40, 10, 10);
-      leaf.addImage(leafImg)
-      leafImgs = Math.round(random(1,2));
-      leaf.scale = 0.1;
-      leaf.velocityY = 2;
-      leaf.depth = rabbit.depth;
-      rabbit.depth ++;
-    }
-    else if(leafImgs==2){
-      leaf = createSprite(random(50, 350), 40, 10, 10);
-      leaf.addImage(orangeLeaf);
-      leafImgs = Math.round(random(1,2));
-      leaf.scale = 0.1;
-      leaf.velocityY = 2;
-      leaf.depth = rabbit.depth;
-      rabbit.depth ++;
-    }
-  }
+  leaf = createSprite(random(50, 350), 40, 10, 10);
+  leaf.addImage(leafImg)
+  leaf.scale = 0.1;
+  leaf.velocityY = 2;
+  leaf.lifetime = 200;
+  leaf.depth = rabbit.depth;
+  rabbit.depth ++;
 }
+
+function createOrangeLeaves(){
+  orangeLeaf = createSprite(random(50, 350), 40, 10, 10);
+  orangeLeaf.addImage(orangeLeafImg);
+  orangeLeaf.scale = 0.1;
+  orangeLeaf.velocityY = 2;
+  orangeLeaf.lifetime = 200;
+  orangeLeaf.depth = rabbit.depth;
+  rabbit.depth ++;
+}
+
+function createRedLeaves(){
+  redLeaf = createSprite(random(50, 350), 40, 10, 10);
+  redLeaf.addImage(redLeafImg);
+  redLeaf.scale = 0.1;
+  redLeaf.velocityY = 2;
+  redLeaf.lifetime = 200;
+  redLeaf.depth = rabbit.depth;
+  rabbit.depth ++;
+}
+
 
 function createApples(){
   if(frameCount%80===0){
@@ -73,7 +104,8 @@ function createApples(){
     apple.addImage(appleImg);
     apple.scale = 0.1;
     apple.velocityY = 2;
-    apple.depth = rabbit.depth
+    apple.lifetime = 200;
+    apple.depth = rabbit.depth;
     rabbit.depth ++;
   }
 }
